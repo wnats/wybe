1f4ea79fdfa19f07c02882c0e7fafe9129e6c6f3
Stanley Wirian <stanley.wirian@gmail.com>
2023-09-28 23:17:13 +1000

    Modified the entity proc names so one could call them directly from source

--- src/Normalise.hs
+++ src/Normalise.hs
@@ -1305,7 +1305,7 @@ entityCreateItem :: Visibility -> ProcName -> TypeSpec -> [Placed Param]
                     -> [FieldInfo] -> Int -> OptPos -> [MergedAttrNames]
                     -> Item
 entityCreateItem vis entityName typeSpec params fields size pos keyNames =
-    ProcDecl vis (inlineModifiers (ConstructorProc procName) Det)
+    ProcDecl vis (noInlineModifiers (ConstructorProc procName) Det)
         (ProcProto procName protoParams resSet)
         (entityAllocStmt typeSpec size pos
             : entityFillAttrsStmts typeSpec attrFieldInfos pos
@@ -1314,7 +1314,7 @@ entityCreateItem vis entityName typeSpec params fields size pos keyNames =
         )
         pos
     where
-        procName = specialName2 createName entityName
+        procName = entityCreateName
         protoParams = 
             (placedApply 
                 (\p -> maybePlace p {paramFlow=ParamIn, paramFlowType=Ordinary})
@@ -1344,7 +1344,7 @@ entityCreateItem vis entityName typeSpec params fields size pos keyNames =
 -- | Generate getter code for an entity
 entityGetItems :: Visibility -> TypeSpec -> Int -> FieldInfo -> Item
 entityGetItems vis etyType etySize (FieldInfo fieldName pos _ fieldType rep offset _) =
-    ProcDecl vis (inlineModifiers (GetterProc fieldName fieldType) Det)
+    ProcDecl vis (noInlineModifiers (GetterProc fieldName fieldType) Det)
         (ProcProto procName protoParams resSet) [stmt] pos
     where
         procName = entityGetterName fieldName
@@ -1369,7 +1369,7 @@ entityGetItems vis etyType etySize (FieldInfo fieldName pos _ fieldType rep offs
 entitySetItems :: Visibility -> TypeSpec -> Int -> EntityModifierDict
                  -> FieldInfo -> Item
 entitySetItems vis etyType etySize modDict (FieldInfo fieldName pos _ fieldType rep offset _) =
-    ProcDecl vis (inlineModifiers (SetterProc fieldName fieldType) Det)
+    ProcDecl vis (noInlineModifiers (SetterProc fieldName fieldType) Det)
         (ProcProto procName protoParams resSet)
         (deleteStmts ++ defaultStmt:insertStmts)
         pos
@@ -1589,14 +1589,20 @@ setName = "set"
 pointerName :: VarName -> VarName
 pointerName = specialName
 
-entityGetterName :: MergedAttrNames -> Ident
-entityGetterName = specialName2 getName
+entityCreateName :: ProcName
+entityCreateName = createName
 
-entitySetterName :: MergedAttrNames -> Ident
-entitySetterName = specialName2 setName
+entityGetterName :: MergedAttrNames -> ProcName
+entityGetterName = entityProcName getName
+
+entitySetterName :: MergedAttrNames -> ProcName
+entitySetterName = entityProcName setName
 
 unplacedVarGetSetDb :: Placed Exp
 unplacedVarGetSetDb = Unplaced $ varGetSet dbResourceName Ordinary
+
+entityProcName :: Ident -> MergedAttrNames -> ProcName
+entityProcName command key = command ++ '_':key
 
 -- |The name of the variable holding a record
 recName :: Ident
